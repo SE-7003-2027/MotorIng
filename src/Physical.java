e2b843b0ad0b79b842aaafb6434a4981c92628ad
fabrizio_pimentel <fabriziopimentel@ciencias.unam.mx>
2026-09-20 15:36:14 -0600

    feat: implement Scenery, also created very very basic World Physical and Input, good enough only to make Scenery compile lol

---
+++ src/Physical.java
@@ -0,0 +1,11 @@
+public class Physical {
+
+    private int x;
+    private int y;
+    private char sprite;
+
+    public void updatePos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/Scenery.java
@@ -0,0 +1,22 @@
+public class Scenery {
+
+    private World world;
+    private Physical[] physical;
+    private char[] sprites;
+
+    public void show() {
+        // Mostrar el mundo
+    }
+
+    public void update(Input input) {
+        // Actualizar objetos y mundo
+    }
+
+    public void changeWorld(World world) {
+        this.world = world;
+    }
+
+    public void start() {
+        // Ciclo principal del motor
+    }
+}
